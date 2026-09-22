4b337c11563a4246a71f53174885b5421d5cd310
Prashant S N <139563804+prashantsn01@users.noreply.github.com>
2026-09-22 10:47:29 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,7 +1,7 @@
-const API_BASE_URL = "https://budget-management-bgal.onrender.com/api";
+const API_BASE_URL = "https://budget-management-backend-6q51.onrender.com/api";
 
 export const API_URLS = {
   auth: `${API_BASE_URL}/auth`,
   budget: `${API_BASE_URL}/budget`,
   transaction: `${API_BASE_URL}/transaction`,
-}; 
+};
